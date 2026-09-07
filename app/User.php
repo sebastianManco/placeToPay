9e0556ae859cdef5f055a54c1388f07372ef695d
Sebastián Manco Valencia <sebastian.manco1997@gmail.com>
2026-09-07 17:16:36 -0500

    feat(auth): make User inherit from Authenticatable and add test suite

--- app/User.php
+++ app/User.php
@@ -2,9 +2,13 @@
 
 namespace App;
 
+use App\Models\User as UserModel;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+
 /**
  * @deprecated Use App\Models\User instead.
  */
-class User extends \App\Models\User
+class User extends UserModel implements AuthenticatableContract
 {
 }
+
